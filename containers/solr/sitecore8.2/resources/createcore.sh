1e3f2d02fce6158e2a2dec01bf93173dcb6bcf1f
Thomas Stern <ts@pentia.dk>
2016-09-29 22:44:13 +0200

    added debug text to script

--- containers/solr/sitecore8.2/resources/createcore.sh
+++ containers/solr/sitecore8.2/resources/createcore.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 name=$1
-echo "Creating core"
+echo "Creating core $name"
 cp -r /opt/solr/server/solr/configsets/basic_configs/ /opt/solr/server/solr/$name && \
 echo name=$name > /opt/solr/server/solr/$name/core.properties
-echo "Done Creating core"
+echo "Done Creating core $name"
+exit
