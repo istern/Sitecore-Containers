93f89b0ef52330e274856c88c10263686736b21e
Thomas Stern <ts@pentia.dk>
2017-05-31 21:34:21 +0200

    fixed rtool ti vs2017

--- containers/windows/iis-rtools/Resources/Start-Remote.Debug.ps1
+++ containers/windows/iis-rtools/Resources/Start-Remote.Debug.ps1
@@ -1,5 +1,1 @@
-Write-Host ("{0}: Starting 'msvsmon.exe'..." -f [DateTime]::Now.ToString("HH:mm:ss:fff"))
-
-& "C:\Program Files\Microsoft Visual Studio 14.0\Common7\IDE\Remote Debugger\x86\msvsmon.exe" /silent /noauth /anyuser;
-
-Write-Host ("{0}: Started 'msvsmon.exe' on port 4020." -f [DateTime]::Now.ToString("HH:mm:ss:fff"))
+& "C:\Program Files\Microsoft Visual Studio 15.0\Common7\IDE\Remote Debugger\x86\msvsmon.exe" /nostatus /silent /noauth /anyuser /nosecuritywarn /timeout:2147483646;
