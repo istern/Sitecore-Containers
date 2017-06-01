4f0ffd826ef31da1e3089c9d30142c7067a0e822
Thomas Stern <ts@pentia.dk>
2017-06-01 09:51:58 +0200

    fixed rtoole install

--- containers/windows/iis-rtools/Resources/Start-Remote.Debug.ps1
+++ containers/windows/iis-rtools/Resources/Start-Remote.Debug.ps1
@@ -1,1 +1,1 @@
-& "C:\Program Files\Microsoft Visual Studio 15.0\Common7\IDE\Remote Debugger\x86\msvsmon.exe" /nostatus /silent /noauth /anyuser /nosecuritywarn /timeout:2147483646;
+& "C:\Program Files\Microsoft Visual Studio 15.0\Common7\IDE\Remote Debugger\x64\msvsmon.exe" /nostatus /silent /noauth /anyuser /nosecuritywarn /timeout:2147483646;
